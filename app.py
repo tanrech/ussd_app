64bd5650dcec36fe7317685fe4f146a088e2dceb
Raymond <hirwaraymond321@gmail.com>
2025-05-03 22:32:58 +0200

    Initial commit: added login and dashboard templates, updated requirements.txt

--- app.py
+++ app.py
@@ -103,6 +103,7 @@ def ussd():
 # ✅ Run the app
 import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
